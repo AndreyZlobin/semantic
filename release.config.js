9c5dbac8477721d400c150acd7d9d04308d9c7db
Andrey <zlobin.andy@gmail.com>
2023-09-22 10:13:04 +0300

    major: rwerwer

--- release.config.js
+++ release.config.js
@@ -50,6 +50,7 @@ module.exports = {
         '@semantic-release/changelog',
         {
           changelogFile: 'CHANGELOG.md',
+          changelogTitle: "# Changelog"
         },
       ],
       ['@semantic-release/github'],
@@ -61,11 +62,4 @@ module.exports = {
         }
       ]
     ],
-    
-    // "plugins": [
-    //   "@semantic-release/commit-analyzer",
-    //   "@semantic-release/release-notes-generator",
-    //   "@semantic-release/npm",
-
-    // ]
 }
